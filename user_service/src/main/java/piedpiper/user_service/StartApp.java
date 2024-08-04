56f505a8a0733c9a46b79fc18af8bc82d8c3b565
Oleh Strokan <oleg14ua71@gmail.com>
2024-08-04 22:58:38 +0200

    v0.2.4. add docker compose, minor update

--- user_service/src/main/java/piedpiper/user_service/StartApp.java
+++ user_service/src/main/java/piedpiper/user_service/StartApp.java
@@ -3,7 +3,9 @@ package piedpiper.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 @PropertySource("file:${user.dir}/user_service/.env")
 public class StartApp {
