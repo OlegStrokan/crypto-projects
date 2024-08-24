617ae3401458ee9a0f0326c537f68aa1c8e8e0ab
Oleh Strokan <oleg14ua71@gmail.com>
2024-08-24 21:14:43 +0200

    app-world: stacking - finish contract

--- app-world/stacking/src/Stacking.sol
+++ app-world/stacking/src/Stacking.sol
@@ -14,21 +14,30 @@ contract Staking {
     mapping(address => Stake) public stakes;
 
     ERC20 public token;
-    address public poolAddress;
+    address public owner;
 
     error StackingAmountZero();
     error InsufficientTokenBalance();
     error InsufficientTokenAllowance();
+    error InsufficientReedemAmount();
     error TransferFailed();
     error RewardTransferFailed();
     error NoContributionToReward();
+    error NoStakedTokens();
     error ReedemAmountZero();
+    error OnlyOwnerFunctional();
 
-    event Stake(address indexed contributor, uint256 amount);
+    event StakeTokens(address indexed contributor, uint256 amount);
     event ClaimInterest(address indexed contributor, uint256 rewards);
+    event Reedem(address indexed contribuor, uint256 amount);
 
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert OnlyOwnerFunctional();
+        _;
+    }
     constructor(address _token) {
         token = ERC20(_token);
+        owner = msg.sender;
     }
 
     // allows users to stake tokens
@@ -49,57 +58,78 @@ contract Staking {
         stakes[msg.sender].stakedAmount += amount;
         stakes[msg.sender].createdAt = block.timestamp;
 
-        emit Stake(msg.sender, amount);
+        emit StakeTokens(msg.sender, amount);
     }
 
     // allows users to reedem staked tokens
     function reedem(uint256 amount) public {
         if (amount == 0) revert ReedemAmountZero();
-        uin256 stakedTokens = stakes[msg.sender].stakedToken;
+        uint256 stakedTokens = stakes[msg.sender].stakedAmount;
         if (amount > stakedTokens) revert InsufficientReedemAmount();
 
-        if(!token.transferFrom(stakes[msg.sender].stakedAmount, msg.sender)) revert TransferFailed();
+        if (
+            !token.transferFrom(
+                address(this),
+                msg.sender,
+                stakes[msg.sender].stakedAmount
+            )
+        ) revert TransferFailed();
+
+        stakes[msg.sender].stakedAmount = 0;
+
+        emit Reedem(msg.sender, amount);
     }
 
     // transfers rewards to staker
     function claimInterest() public {
-        if (stakes[msg.sender].stakedAmount == 0) revert NoContributionToReward();
+        if (stakes[msg.sender].stakedAmount == 0)
+            revert NoContributionToReward();
 
-         _claimRewards(stales[msg.sender].stakedAmount, msg.sender);
+        _claimRewards(stakes[msg.sender].stakedAmount, msg.sender);
         stakes[msg.sender].stakedAmount = 0;
-
     }
 
     // returns the accrued interest
-    function getAccruedInterest(address user) public returns (uint256) {}
+    function getAccruedInterest(address user) public view returns (uint256) {
+        Stake memory stakeInfo = stakes[user];
+        if (stakeInfo.stakedAmount == 0) revert NoStakedTokens();
+
+        uint256 rewards = _calculateRewards(
+            stakeInfo.stakedAmount,
+            stakeInfo.createdAt
+        );
+        return rewards;
+    }
 
     // allows owner to collect all the staked tokens
-    function sweep() public {}
+    function sweep() public onlyOwner {
+        uint256 stakedAmount = token.balanceOf(address(this));
+
+        if (!token.transfer(owner, stakedAmount)) revert TransferFailed();
+    }
 
     //----------------------------helpers---------------------------//
     function _calculateRewards(
         uint256 stakedAmount,
         uint256 createdAt
-    ) private pure returns (uint256 rewards) {
+    ) private view returns (uint256 rewards) {
         uint256 timePassed = block.timestamp - createdAt;
         if (timePassed < 1 days) return 0;
         if (timePassed >= 1 days && timePassed < 1 weeks)
-            return amount + ((amount * 1) / 100);
+            return stakedAmount + ((stakedAmount * 1) / 100);
         if (timePassed >= 1 weeks && timePassed < 30 days)
-            return amount + ((amount * 10) / 100);
-        if (timePassed >= 30 days) return amount + ((amount * 50) / 100);
+            return stakedAmount + ((stakedAmount * 10) / 100);
+        if (timePassed >= 30 days)
+            return stakedAmount + ((stakedAmount * 50) / 100);
     }
 
-    function _claimRewards(
-        uin256 stakedAmount,
-        address sender
-    ) private {
-          uint256 stakedAmount = stakes[msg.sender].stakedAmount;
-         uint256 rewards = _calculateRewards(stakedAmount, sender);
+    function _claimRewards(uint256 _stakedAmount, address _sender) private {
+        uint256 createdAt = stakes[_sender].createdAt;
+        uint256 rewards = _calculateRewards(_stakedAmount, createdAt);
 
-            if (!token.transferFrom(address(this), sender,, rewards))
-                revert RewardTransferFailed();
+        if (!token.transferFrom(address(this), _sender, rewards))
+            revert RewardTransferFailed();
 
-        emit ClaimInterest(msg.sender, rewards);
+        emit ClaimInterest(_sender, rewards);
     }
 }
