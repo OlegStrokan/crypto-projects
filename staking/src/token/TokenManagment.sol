7b0a9b8c31cbc338e768711eba839f177d8cce2c
Oleh Strokan <oleg14ua71@gmail.com>
2024-08-28 10:45:01 +0200

    staking: update token managment contract

--- staking/src/token/TokenManagment.sol
+++ staking/src/token/TokenManagment.sol
@@ -1,24 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface ERC20 {
-    function transfer(
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract TokenManagment {
     address public owner;
     address public tokenAddress;
-    uint256 public tokenSalePrice;
+    uint256 public tokenSalePrice; // Price in wei per token
+    uint256 public soldTokens;
 
     modifier onlyOwner() {
-        require(
-            msg.sender == owner,
-            "Only contract owner can perform this action"
-        );
+        require(msg.sender == owner, "Only owner can perform this action");
         _;
     }
 
@@ -28,32 +20,68 @@ contract TokenManagment {
         tokenSalePrice = _tokenSalePrice;
     }
 
-    function updateTokenSalePrice(uint256 _tokenSalePrice) public onlyOwner {
-        tokenSalePrice = _tokenSalePrice;
-    }
-
+    // Allows users to buy tokens
     function buyToken(uint256 _tokenAmount) public payable {
-        uint256 amountRequired = _tokenAmount * tokenSalePrice;
-        require(msg.value >= amountRequired, "Insufficient Ether provided");
+        uint256 requiredEth = _tokenAmount * tokenSalePrice;
+        require(msg.value == requiredEth, "Incorrect ETH amount sent");
 
         ERC20 token = ERC20(tokenAddress);
-        uint256 contractBalance = token.balanceOf(address(this));
+        uint256 tokenAmountWithDecimals = _tokenAmount * 10 ** token.decimals();
+
         require(
-            _tokenAmount <= contractBalance,
+            token.balanceOf(address(this)) >= tokenAmountWithDecimals,
             "Insufficient tokens available"
         );
-
         require(
-            token.transfer(msg.sender, _tokenAmount),
+            token.transfer(msg.sender, tokenAmountWithDecimals),
             "Token transfer failed"
         );
+
         payable(owner).transfer(msg.value);
+        soldTokens += _tokenAmount;
     }
 
-    function withdrawAllTokens() public onlyOwner {
+    // Allows users to sell tokens and receive ETH
+    function sellToken(uint256 _tokenAmount) public {
         ERC20 token = ERC20(tokenAddress);
-        uint256 balance = token.balanceOf(address(this));
-        require(balance > 0, "No tokens to withdraw");
-        require(token.transfer(owner, balance), "Token withdrawal failed");
+        uint256 tokenAmountWithDecimals = _tokenAmount * 10 ** token.decimals();
+        uint256 ethAmount = _tokenAmount * tokenSalePrice;
+
+        require(
+            token.balanceOf(msg.sender) >= tokenAmountWithDecimals,
+            "Insufficient token balance"
+        );
+        require(
+            token.allowance(msg.sender, address(this)) >=
+                tokenAmountWithDecimals,
+            "Insufficient token allowance"
+        );
+        require(
+            address(this).balance >= ethAmount,
+            "Insufficient ETH balance in contract"
+        );
+
+        require(
+            token.transferFrom(
+                msg.sender,
+                address(this),
+                tokenAmountWithDecimals
+            ),
+            "Token transfer failed"
+        );
+        payable(msg.sender).transfer(ethAmount);
+    }
+
+    // Allows the contract owner to withdraw ETH from the contract
+    function withdrawETH(uint256 amount) public onlyOwner {
+        require(address(this).balance >= amount, "Insufficient ETH balance");
+        payable(owner).transfer(amount);
+    }
+
+    // Allows the contract owner to withdraw all ETH from the contract
+    function withdrawAllETH() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No ETH to withdraw");
+        payable(owner).transfer(balance);
     }
 }
